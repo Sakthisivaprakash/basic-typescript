9a56b86ced427304f6ce287957322112a0b6adf4
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2023-01-24 00:16:57 +0530

    working with literal types

--- app.ts
+++ app.ts
@@ -1,15 +1,28 @@
-function combine(ip1: number | string, ip2: number | string) {
-    let result;
-    if(typeof ip1 === 'number' && typeof ip2 === 'number') {
-        result = ip1 + ip2;
-    } else {
-        result = ip1.toString() + ip2.toString();
-    }
-    return result;
+function combine(
+  ip1: number | string,
+  ip2: number | string,
+  resultConcersion: 'as-number' | 'as-text'
+) {
+  let result;
+  if (typeof ip1 === "number" && typeof ip2 === "number" || resultConcersion === 'as-number') {
+    result = +ip1 + +ip2;
+  } else {
+    result = ip1.toString() + ip2.toString();
+  }
+  return result;
+//   if(resultConcersion === 'as-number') {
+//     return +result
+//   } else {
+//     return result.toString();
+//   }
 }
 
-const combinedAges = combine(30,26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Sakthi', 'Geetha');
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+
+const combinedNames = combine("Sakthi", "Geetha", 'as-text');
 console.log(combinedNames);
